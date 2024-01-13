4819c1b6591af18ee8fb0dd904cc93d72a20a113
E8 Labs <83865827+E8-Labs@users.noreply.github.com>
2024-01-13 16:37:57 +0500

    terms use client removed

--- app/dashboard/terms/page.js
+++ app/dashboard/terms/page.js
@@ -1,10 +1,10 @@
-'use client'
+// 'use client'
 import React from 'react'
-import { useSearchParams } from 'next/navigation'
+// import { useSearchParams } from 'next/navigation'
 
 function page() {
-  const searchParams = useSearchParams()
-  console.log(searchParams.get('search')) // Logs "search"
+  // const searchParams = useSearchParams()
+  // console.log(searchParams.get('search')) // Logs "search"
   return (
     <div>
         This is terms page
